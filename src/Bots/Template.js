0e44a6b3a49d4cb4fc8eb2869e1f9ae119449da5
upwingo <support@upwingo.com>
2018-08-05 01:59:20 +0300

    Template

--- src/Bots/Template.js
+++ src/Bots/Template.js
@@ -1,11 +1,15 @@
 function BotTemplate(upwingo) {
 
+    const channels = [];
+
     const onTick = (data) => {
         // TODO: Implement onTick() function
     };
 
     this.run = () => {
         // TODO: Implement run() function
+
+        upwingo.ticker(channels, onTick);
     };
 }
 
